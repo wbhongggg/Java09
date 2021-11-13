04ecd43a3ab74bb514b7f730e107fe9e92d729ae
wbhongggg <92263847+wbhongggg@users.noreply.github.com>
2021-11-13 19:02:00 +0800

    Add files via upload

---
+++ Class01_ab.java
@@ -0,0 +1,16 @@
+package Java09;
+class CRectangle
+{
+	int width;
+	int height;
+	public CRectangle(int w,int h)
+	{
+		width=w;
+		height=h;
+	}
+	public CRectangle()
+	{
+		width=10;
+		height=8;
+	}
+}
---
+++ Class01_ac.java
@@ -0,0 +1,15 @@
+package Java09;
+class CRectangle
+{
+	int width;
+	int height;
+	public CRectangle(int w,int h)
+	{
+		width=w;
+		height=h;
+	}
+	public CRectangle()
+	{
+		this(10,8);
+	}
+}
