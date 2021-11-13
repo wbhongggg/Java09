5fe363c003af8e9454c4b4d6bd90ecf852a64616
wbhongggg <92263847+wbhongggg@users.noreply.github.com>
2021-11-13 21:16:27 +0800

    Add files via upload

---
+++ Class14.java
@@ -0,0 +1,31 @@
+package Java09;
+class CCircle
+{
+   private static double pi=3.14;
+   private double radius;
+   public CCircle(double r)
+   {
+      radius=r;
+   }
+   public static double compare(CCircle c[])
+   {
+      double max=0.0;
+      for(int i=0;i<c.length;i++)
+         if(c[i].radius>max)
+            max=c[i].radius;
+      return max;
+   }
+}
+public class Class14
+{
+   public static void main(String args[])
+   {
+      CCircle cir[];
+      cir=new CCircle[3];
+      cir[0]=new CCircle(1.0);
+      cir[1]=new CCircle(4.0);
+      cir[2]=new CCircle(2.0);
+      System.out.println("Largest radius = "+CCircle.compare(cir));
+   }
+}
+//建立類別函數，可直接呼叫函數，不必建立物件
